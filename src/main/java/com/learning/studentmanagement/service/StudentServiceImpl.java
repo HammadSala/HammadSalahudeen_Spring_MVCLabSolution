e1b254555b35405b6a4db4531ae08ead87ab4b2d
flash_hammad <flash_hammad@yahoo.co.in>
2022-06-26 13:49:19 +0530

    Ading Screenshot and updatingService class

--- src/main/java/com/learning/studentmanagement/service/StudentServiceImpl.java
+++ src/main/java/com/learning/studentmanagement/service/StudentServiceImpl.java
@@ -8,11 +8,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.learning.studentmanagement.model.Student;
 
-@Component
+@Repository
 public class StudentServiceImpl implements StudentInterface {
 
 	// Creating object leveraging SPring
